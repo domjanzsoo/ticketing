a053b6398e7a15c976f50e96c390fb7be2248c31
Zsolt Domjan <domjanzsoo@yahoo.com>
2021-03-14 22:44:04 +0000

    Setting up mongoDB connection,handlebars configuration,  writing basic routes

---
+++ config/db.js
@@ -0,0 +1,18 @@
+const mongoos = require('mongoose');
+
+const connectDB = async () => {
+    try {
+        const conn = await mongoos.connect(process.env.MONGO_URI, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+            useFindAndModify: false
+        })
+
+        console.log(`MongoDB connected: ${conn.connection.host} `)
+    } catch(err) {
+        console.log(err)
+        process.exit(1)
+    }
+}
+
+module.exports = connectDB
--- server.js
+++ server.js
@@ -1,10 +1,28 @@
 const express = require('express')
 const dotenv = require('dotenv')
+const morgan = require('morgan')
+const exphbs = require('express-handlebars');
+const connectDB = require('./config/db')
+const routes = require('./routes')
 
 // Load config
 dotenv.config({ path: './config/config.env' })
 
+//DB connection
+connectDB()
+
 const app = express()
+
+if(process.env.NODE_ENV === 'development'){
+        app.use(morgan('dev'))
+}
+
+// Hanlebars
+app.engine('.hbs', exphbs({defaultLayour: 'main', extname: '.hbs'}))
+app.set('view engine', '.hbs')
+
+// Routes
+app.use('/', require('./routes/index'))
 
 const PORT = process.env.PORT || 5000
 
