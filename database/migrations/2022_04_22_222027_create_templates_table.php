5106cbb953442c7b2d9c80629cee024e15ce9b4e
Zsolt Domjan <domjanzsoo@yahoo.com>
2022-04-22 23:30:30 +0100

    Migration of templates and label_sizes tables

---
+++ database/migrations/2022_04_22_222027_create_templates_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTemplatesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('templates', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('label_size_id');
+            $table->string('name');
+            $table->string('description');
+            $table->timestamps();
+
+            $table->foreign('label_size_id')->references('id')->on('label_sizes');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('templates');
+    }
+}
